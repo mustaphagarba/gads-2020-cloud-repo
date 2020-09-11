42e11dd10c50e96b0a0646679abac67a064acc45
Mustapha Garba <mgarba88@gmail.com>
2020-09-11 19:58:35 +0100

    added screenshots

--- cmds/lab3/bash script for cloud sql lab.sh
+++ cmds/lab3/bash script for cloud sql lab.sh
@@ -3,3 +3,12 @@
 cloud beta compute --project=qwiklabs-gcp-04-05983770a79a instances create bloghost --zone=us-central1-a --machine-type=e2-medium --subnet=default --network-tier=PREMIUM --metadata=startup-script=apt-get\ update$'\n'apt-get\ install\ apache2\ php\ php-mysql\ -y$'\n'service\ apache2\ restart --maintenance-policy=MIGRATE --service-account=63473337628-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=http-server --image=debian-9-stretch-v20200902 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=bloghost --reservation-affinity=any
 
 gcloud compute --project=qwiklabs-gcp-04-05983770a79a firewall-rules create default-allow-http --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
+
+# create storage bucket
+
+export LOCATION=US
+gsutil mb -l $LOCATION gs://$DEVSHELL_PROJECT_ID
+gsutil cp gs://cloud-training/gcpfci/my-excellent-blog.png my-excellent-blog.png
+gsutil cp my-excellent-blog.png gs://$DEVSHELL_PROJECT_ID/my-excellent-blog.png
+gsutil acl ch -u allUsers:R gs://$DEVSHELL_PROJECT_ID/my-excellent-blog.png
+
